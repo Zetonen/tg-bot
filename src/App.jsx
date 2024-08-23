388f113beebabbef044778f11ca90daa20cd4b6a
Seven <76436489+Zetonen@users.noreply.github.com>
2024-08-23 09:33:16 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { GlobalStyles } from "./components/GlobalStyles";
 import { sendGuess } from "./api";
@@ -22,14 +20,6 @@ function App() {
   }, [count]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
