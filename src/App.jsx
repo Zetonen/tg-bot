9e1b7caab1aab88036f1c23234f86321ddcfd6f6
Seven <76436489+Zetonen@users.noreply.github.com>
2024-08-23 09:45:01 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,10 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          style={{ color: "var(--tg-theme-bg-color)" }}
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
