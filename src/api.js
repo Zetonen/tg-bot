e59a39c89cd3dee5b143557c87d300dff9ad8f53
Seven <76436489+Zetonen@users.noreply.github.com>
2024-08-23 09:16:38 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://localhost:3001";
+axios.defaults.baseURL = "http://localhost:3001";
 
 export const sendGuess = async (guess, info) => {
   const response = await axios.post("/guess", { guess, info });
